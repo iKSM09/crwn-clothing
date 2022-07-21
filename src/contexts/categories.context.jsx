9b7eedf7c10dad48f6c9fa41b7c216a1753b0921
Kunal Sharma <kunal.iksm09@gmail.com>
2022-07-21 17:36:28 +0530

    added category-preview and category-item with dynamic routes for shop page

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+
+  // Uploading Data to Firebase's Firestore.
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
