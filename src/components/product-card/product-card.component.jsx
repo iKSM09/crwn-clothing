67f47480358e5929ded1eb58826b3dbe91c928e9
Kunal Sharma <kunal.iksm09@gmail.com>
2022-07-26 14:39:06 +0530

    added redux to the project

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,6 +1,9 @@
-import { useContext } from "react";
+// import { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { CartContext } from "../../contexts/cart.context";
+// import { CartContext } from "../../contexts/cart.context";
+import { selectCartItems } from "../../store/cart/cart.selector";
+import { addItemToCart } from "../../store/cart/cart.action";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../buton/button.component";
 
@@ -12,10 +15,14 @@ import {
 } from "./product-card.styles";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
   const { name, price, imageUrl } = product;
-  const { addItemToCart } = useContext(CartContext);
 
-  const addProductToCart = () => addItemToCart(product);
+  // const { addItemToCart } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
+
+  // const addProductToCart = () => addItemToCart(product);
+  const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
   return (
     <ProductCardContainer>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.compoent";
 
+import { setCategories } from "../../store/categories/category.action";
+
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriseArray = await getCategoriesAndDocuments("categories");
+      dispatch(setCategories(categoriseArray));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
